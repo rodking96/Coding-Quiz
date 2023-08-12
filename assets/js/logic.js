5f207a2ff01876588e22dddd9b7d048ef882fd64
rodking7 <rodking7@imac.mynetworksettings.com>
2023-08-12 11:17:18 -0400

    first commit

---
+++ assets/js/logic.js
@@ -0,0 +1,141 @@
+// variables to keep track of quiz state
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+var questionsEl = document.getElementById('questions');
+var timerEl = document.getElementById('time');
+var choicesEl = document.getElementById('choices');
+var submitBtn = document.getElementById('submit');
+var startBtn = document.getElementById('start');
+var initialsEl = document.getElementById('initials');
+var feedbackEl = document.getElementById('feedback');
+
+function startQuiz() {
+    //hide start screen
+    var startScreenEl = document.getElementById('start-screen');
+    startScreenEl.setAttribute('class', 'hide');
+
+    questionsEl.removeAttribute('class');
+
+    timerId = setInterval(clockTick, 1000);
+
+    timerEl.textContent = time;
+
+    getQuestion();
+}
+
+function getQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+
+    var titleEl = document.getElementById('questions-title');
+    titleEl.textContent = currentQuestion.title;
+
+    choicesEl.innerHTML = '';
+
+    // loop over choices
+    for (var i = 0; i < currentQuestion.choices.length; i++) {
+        var choice = currentQuestion.choices[i];
+        var choiceNode = document.createElement('button');
+        choiceNode.setAttribute('class', 'choice');
+        choiceNode.setAttribute('value', choice);
+
+        choiceNode.textContent = i + 1 + '. ' + choice;
+
+        choicesEl.appendChild(choiceNode);
+    }
+}
+
+function questionClick(event) {
+    var buttonEl = event.target;
+
+    if (!buttonEl.matches('.choice')) {
+        return;
+    }
+
+    if (buttonEl.value !== questions[currentQuestionIndex].answer) {
+        //penalize time
+        time -= 15;
+
+        if (time < 0) {
+            time = 0;
+        }
+        timerEl.textContent = time;
+
+        feedbackEl.textContent = 'Wrong!';
+    } else {
+        feedbackEl.textContent = 'Correct!';
+    }
+
+    feedbackEl.setAttribute('class', 'feedback');
+    setTimeout(function () {
+        feedbackEl.setAttribute('class', 'feedback hide');
+    }, 1000);
+
+    //move to next question
+
+    currentQuestionIndex++;
+
+    if (time <= 0 || currentQuestionIndex === questions.length) {
+        quizEnd ();
+    } else {
+        getQuestion();
+    }
+}
+
+function quizEnd() {
+    //stop timer
+    clearInterval(timerId);
+
+    var endScreenEl = document.getElementById('end-screen');
+    endScreenEl.removeAttribute('class');
+
+    //show final score
+    var finalScoreEl = document.getElementById('final-score');
+    finalScoreEl.textContent = time;
+
+    questionsEl.setAttribute('class', 'hide');
+
+}
+
+function clockTick() {
+    //update time
+    time--;
+    timerEl.textContent = time;
+
+    if (time <= 0) {
+        quizEnd();
+    }
+}
+
+function saveHighscore() {
+    var initials = initialsEl.value.trim();
+
+    if (initials !== '') {
+        var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+
+        var newScore = {
+            score: time,
+            initials: initials
+        };
+
+        highscores.push(newScore);
+        window.localStorage.setItem('highscores', JSON.stringify(highscores));
+
+        window.location.href = "highscores.html";
+    }
+}
+
+function checkForEnter(event) {
+    if (event.key === 'Enter') {
+        saveHighscore();
+    }
+}
+
+submitBtn.onclick = saveHighscore;
+
+startBtn.onclick = startQuiz;
+
+choicesEl.onclick = questionClick;
+
+initialsEl.onkeyup = checkForEnter
---
+++ assets/js/questions.js
@@ -0,0 +1,23 @@
+var questions = [
+    {
+        title: 'Commonly used data types DO NOT include:',
+        choices: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: 'alerts',
+    },
+    {
+        title: 'Arrays in Javascript can be used to store ___.',
+        choices: ['strings', 'booleans', 'numbers', 'all of the above'],
+        answer: 'all of the above',
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+      },
+      {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+      },
+
+];
